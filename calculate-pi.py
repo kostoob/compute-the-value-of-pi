7b978826e7fa77a61e5e4f9a10f1153c44fc52ed
Kaustubh <45540855+kostoob@users.noreply.github.com>
2021-06-02 02:06:57 +0530

    Create calculate-pi.py

---
+++ calculate-pi.py
@@ -0,0 +1,17 @@
+import random
+
+def compute_pi(n):
+    points_in_circle = 0
+    points_in_square = 0
+    for i in range(n):
+        x = random.uniform(0,1)
+        y = random.uniform(0,1)
+        distance = x**2 + y**2
+        if distance <= 1:
+            points_in_circle += 1
+        points_in_square += 1
+    pi = (points_in_circle*4)/points_in_square
+    print(pi)
+
+
+compute_pi(100000)
